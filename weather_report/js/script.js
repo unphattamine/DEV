eca31be8e51d707a7a5f057c6be6a0406ce86455
Samuel Imrich <simrich@ipsoft.com>
2023-06-17 14:00:22 +0200

    working weather present

--- weather_report/js/script.js
+++ weather_report/js/script.js
@@ -12,13 +12,16 @@ const validateCity = () => {
     }
 }
 
-async function weatherCall(city){
+function weatherCall(city){
     let url = "https://www.meteosource.com/api/v1/free/point?place_id=" + city + "&sections=all&timezone=UTC&language=en&units=metric&key=fnaddym2jcdwwc7xw08lafth1t2b4sd1vpj6hwu0";
-        const response = await fetch(url);
-        const jsonData = await response.json();
-        console.log(jsonData);
-        console.log(jsonData.current.summary, jsonData.current.temperature)
-        return weather;
+    fetch(url)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(weather) {
+        console.log(weather);
+        document.getElementById("weather").innerHTML =  weather.current.summary + " " +  weather.current.temperature + "\°C" + ", Wind speed " + weather.current.wind.speed + ", Direction:" + weather.current.wind.dir + ", Angle: " + weather.current.wind.angle;
+    }) 
 }
 
 
@@ -30,7 +33,8 @@ function main(){
     if (validateCity() == 0){
         return;
     }
-    const current = weatherCall(city);
-    printWeather(current);
+    weatherCall(city);
+    //const current = weatherCall(city);
+    //printWeather(current);
 
 }
