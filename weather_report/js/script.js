853cfc98bfc7dfede542091474d4c5615f153d71
Samuel Imrich <simrich@ipsoft.com>
2023-06-17 12:43:56 +0200

    Able to fetch weather data

--- weather_report/js/script.js
+++ weather_report/js/script.js
@@ -1,32 +1,36 @@
-function getCity(){
+const getCity = () => {
     let city = document.getElementById("city").value;
     console.log(city);
     return city;
 }
 
-function validateCity(){
+const validateCity = () => {
     city = getCity();
     if (!city.match(/^[A-Za-z]+$/)){
-        alert("Invalid city");
-        return;
+        alert("City is invalid!");
+        return 0;
     }
 }
+
 async function weatherCall(city){
     let url = "https://www.meteosource.com/api/v1/free/point?place_id=" + city + "&sections=all&timezone=UTC&language=en&units=metric&key=fnaddym2jcdwwc7xw08lafth1t2b4sd1vpj6hwu0";
         const response = await fetch(url);
         const jsonData = await response.json();
         console.log(jsonData);
-        console.log(url);
-        return response;
+        console.log(jsonData.current.summary, jsonData.current.temperature)
+        return weather;
 }
 
-function printWeather(){
-    document.getElementById("weather").innerHTML = "test";
+
+function printWeather(current){
+    document.getElementById("weather").innerHTML = current;
 }
 
 function main(){
-    validateCity();
-    weatherCall(city);
-    printWeather();
+    if (validateCity() == 0){
+        return;
+    }
+    const current = weatherCall(city);
+    printWeather(current);
 
 }
